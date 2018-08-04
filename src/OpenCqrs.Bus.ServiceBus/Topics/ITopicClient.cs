13a3259ced970c295816b1a27b917f9bdb31f8e5
Luca Briguglia <luca.briguglia@outlook.com>
2018-08-04 16:34:31 +0100

    #21

---
+++ src/OpenCqrs.Bus.ServiceBus/Topics/ITopicClient.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace OpenCqrs.Bus.ServiceBus.Topics
+{
+    public interface ITopicClient
+    {
+        Task SendAsync<TMessage>(TMessage message) where TMessage : IBusTopicMessage;
+    }
+}
---
+++ src/OpenCqrs/Bus/IBusTopicMessage.cs
@@ -0,0 +1,7 @@
+﻿namespace OpenCqrs.Bus
+{
+    public interface IBusTopicMessage : IBusMessage
+    {
+        string TopicName { get; set; }
+    }
+}
