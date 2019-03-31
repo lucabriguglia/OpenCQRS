fd75d625c806364cc3469aeb5343e3d7a5d963b5
Luca Briguglia <luca.briguglia@outlook.com>
2019-03-31 11:19:15 +0100

    Add basic queue and topic clients implementation

---
+++ src/OpenCqrs.Bus.RabbitMQ/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OpenCqrs.Bus.RabbitMQ.Factories;
+using OpenCqrs.Bus.RabbitMQ.Queues;
+using OpenCqrs.Bus.RabbitMQ.Topics;
+using OpenCqrs.Extensions;
+
+// ReSharper disable InconsistentNaming
+
+namespace OpenCqrs.Bus.RabbitMQ.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the service bus provider.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="configuration">The configuration.</param>
+        /// <returns></returns>
+        public static IOpenCqrsServiceBuilder AddRabbitMQProvider(this IOpenCqrsServiceBuilder builder, IConfiguration configuration)
+        {
+            builder.Services
+                .Configure<ServiceBusConfiguration>(configuration.GetSection(Constants.ServiceBusConfigurationSection));
+
+            builder.Services
+                .AddTransient<IQueueClient, QueueClient>()
+                .AddTransient<ITopicClient, TopicClient>()
+                .AddTransient<IMessageFactory, MessageFactory>();
+
+            return builder;
+        }
+    }
+}
