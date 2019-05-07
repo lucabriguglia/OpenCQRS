1ff6a32ac6768f08e41eb783fbbe76cd40b1f655
Juan Carlos Andreu <andreujuanc@gmail.com>
2019-05-07 18:59:45 +0200

    More properties implemented on the Service Bus Message.

--- src/OpenCqrs.Bus.ServiceBus/Factories/MessageFactory.cs
+++ src/OpenCqrs.Bus.ServiceBus/Factories/MessageFactory.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.Generic;
+using System.Text;
 using Microsoft.Azure.ServiceBus;
 using Newtonsoft.Json;
 
@@ -6,14 +7,26 @@ namespace OpenCqrs.Bus.ServiceBus.Factories
 {
     public class MessageFactory : IMessageFactory
     {
+        public static readonly string AssemblyQualifiedNamePropertyName = "AssemblyQualifiedName";
         /// <inheritdoc />
         public Message CreateMessage<TMessage>(TMessage message) where TMessage : IBusMessage
         {
             var json = JsonConvert.SerializeObject(message);
-            var serviceBusMessage = new Message(Encoding.UTF8.GetBytes(json));
+            var serviceBusMessage = new Message(Encoding.UTF8.GetBytes(json))
+            {
+                ContentType = "application/json"
+            };
 
             if (message.ScheduledEnqueueTimeUtc.HasValue)
                 serviceBusMessage.ScheduledEnqueueTimeUtc = message.ScheduledEnqueueTimeUtc.Value;
+            if (string.IsNullOrWhiteSpace(message.Label))
+                serviceBusMessage.Label = message.Label;
+            if (!string.IsNullOrWhiteSpace(message.SessionId))
+                serviceBusMessage.SessionId = message.SessionId;
+            if (!string.IsNullOrWhiteSpace(message.CorrelationId))
+                serviceBusMessage.CorrelationId = message.CorrelationId;
+
+            serviceBusMessage.UserProperties.Add(new KeyValuePair<string, object>(AssemblyQualifiedNamePropertyName, message.GetType().AssemblyQualifiedName));
 
             return serviceBusMessage;
         }
