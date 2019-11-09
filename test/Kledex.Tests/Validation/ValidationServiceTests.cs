32387f504dea8da27bddc2fd025aae2810aa9798
Luca Briguglia <lubrigug@microsoft.com>
2019-11-09 18:39:55 +0000

    Add tests

---
+++ test/Kledex.Tests/Validation/ValidationServiceTests.cs
@@ -0,0 +1,113 @@
+﻿using AutoFixture;
+using Kledex.Tests.Fakes;
+using Kledex.Validation;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Kledex.Tests.Validation
+{
+    [TestFixture]
+    public class ValidationServiceTests
+    {
+        private IValidationService _sut;
+        private Mock<IValidationProvider> _validationProviderMock;
+        private CreateAggregate _createAggregate;
+        private ValidationResponse _validationResponse;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _createAggregate = new Fixture().Create<CreateAggregate>();
+            _validationResponse = new Fixture().Build<ValidationResponse>()
+                .With(x => x.Errors, new List<ValidationError>())
+                .Create();
+
+            _validationProviderMock = new Mock<IValidationProvider>();
+            _validationProviderMock
+                .Setup(x => x.ValidateAsync(_createAggregate))
+                .ReturnsAsync(_validationResponse);
+            _validationProviderMock
+                .Setup(x => x.Validate(_createAggregate))
+                .Returns(_validationResponse);
+
+            _sut = new ValidationService(_validationProviderMock.Object);
+        }
+
+        [Test]
+        public void ValidateAsync_ThrowsException_WhenCommandIsNull()
+        {
+            _createAggregate = null;
+            Assert.ThrowsAsync<ArgumentNullException>(async () => await _sut.ValidateAsync(_createAggregate));
+        }
+
+        [Test]
+        public void Validate_ThrowsException_WhenCommandIsNull()
+        {
+            _createAggregate = null;
+            Assert.ThrowsAsync<ArgumentNullException>(async () => await _sut.ValidateAsync(_createAggregate));
+        }
+
+        [Test]
+        public async Task ValidateAsync_CallsProvider()
+        {
+            await _sut.ValidateAsync(_createAggregate);
+            _validationProviderMock.Verify(x => x.ValidateAsync(_createAggregate), Times.Once);
+        }
+
+        [Test]
+        public void Validate_CallsProvider()
+        {
+            _sut.Validate(_createAggregate);
+            _validationProviderMock.Verify(x => x.Validate(_createAggregate), Times.Once);
+        }
+
+        [Test]
+        public void ValidateAsync_ThrowsException_WhenValidationFails()
+        {
+            _validationResponse = new Fixture().Build<ValidationResponse>()
+                .With(x => x.Errors, new List<ValidationError>
+                {
+                    new ValidationError
+                    {
+                        PropertyName = "Something",
+                        ErrorMessage = "Blah blah blah..."
+                    }
+                })
+                .Create();
+
+            _validationProviderMock
+                .Setup(x => x.ValidateAsync(_createAggregate))
+                .ReturnsAsync(_validationResponse);
+
+            _sut = new ValidationService(_validationProviderMock.Object);
+
+            Assert.ThrowsAsync<ValidationException>(async () => await _sut.ValidateAsync(_createAggregate));
+        }
+
+        [Test]
+        public void Validate_ThrowsException_WhenValidationFails()
+        {
+            _validationResponse = new Fixture().Build<ValidationResponse>()
+                .With(x => x.Errors, new List<ValidationError>
+                {
+                    new ValidationError
+                    {
+                        PropertyName = "Something",
+                        ErrorMessage = "Blah blah blah..."
+                    }
+                })
+                .Create();
+
+            _validationProviderMock
+                .Setup(x => x.Validate(_createAggregate))
+                .Returns(_validationResponse);
+
+            _sut = new ValidationService(_validationProviderMock.Object);
+
+            Assert.Throws<ValidationException>(() => _sut.Validate(_createAggregate));
+        }
+    }
+}
