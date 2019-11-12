63d909932d410af242a5ba235fa7831a855ec30b
Luca Briguglia <lubrigug@microsoft.com>
2019-11-12 15:30:01 +0000

    Add redis cache provider project

---
+++ src/Kledex.Caching.Redis/RedisCacheProvider.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Kledex.Caching.Redis
+{
+    public class RedisCacheProvider : ICacheProvider
+    {
+        public Task<T> GetAsync<T>(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetAsync(string key, int cacheTime, object data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsSetAsync(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get<T>(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Set(string key, int cacheTime, object data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsSet(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(string key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Kledex.Caching.Redis/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Kledex.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kledex.Caching.Redis
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IKledexServiceBuilder AddRedisCacheProvider(this IKledexServiceBuilder builder)
+        {
+            builder.Services.AddTransient<ICacheProvider, RedisCacheProvider>();
+
+            return builder;
+        }
+    }
+}
