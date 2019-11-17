b25e4ada57b47187fe6ab6f785e82669d144ecb8
Luca Briguglia <lubrigug@microsoft.com>
2019-11-17 15:22:53 +0000

    Update IStoreProvider

--- src/Kledex/Domain/IStoreProvider.cs
+++ src/Kledex/Domain/IStoreProvider.cs
@@ -7,7 +7,7 @@ namespace Kledex.Domain
     public interface IStoreProvider
     {
         /// <summary>
-        /// Saves the event asynchronous.
+        /// Saves the domain data asynchronously.
         /// </summary>
         /// <param name="aggregateRootId"></param>
         /// <param name="command">The command.</param>
@@ -17,14 +17,14 @@ namespace Kledex.Domain
         Task SaveAsync(Type aggregateType, Guid aggregateRootId, IDomainCommand command, IEnumerable<IDomainEvent> events);
 
         /// <summary>
-        /// Gets the events asynchronous.
+        /// Gets the events asynchronously.
         /// </summary>
         /// <param name="aggregateId">The aggregate identifier.</param>
         /// <returns></returns>
         Task<IEnumerable<DomainEvent>> GetEventsAsync(Guid aggregateId);
 
         /// <summary>
-        /// Saves the event.
+        /// Saves the domain data.
         /// </summary>
         /// <param name="aggregateRootId"></param>
         /// <param name="command">The command.</param>
