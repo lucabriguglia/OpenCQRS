808f52b86233efb41b5addd00347acfe64bf588f
Luca Briguglia <lubrigug@microsoft.com>
2019-12-19 10:50:09 +0000

    Update service bus message factory

--- src/Kledex/Bus/IBusMessage.cs
+++ src/Kledex/Bus/IBusMessage.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Kledex.Bus
 {
     public interface IBusMessage
     {
-        [Obsolete("Set add a new entry in Properties with the key ScheduledEnqueueTimeUtc instead")]
-        DateTime? ScheduledEnqueueTimeUtc { get; set; }
         IDictionary<string, object> Properties { get; set; }
     }
 }
